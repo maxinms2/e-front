6968fae1ac8a5ba2d594a7d7f0c399cc0ffb7d9b
Edgar Mejia <you@example.com>
2024-06-25 22:03:23 -0600

    Administración de productos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './components/home/home.component';
 import { HttpClientModule } from '@angular/common/http';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { HeaderAdminComponent } from './components/header-admin/header-admin.component';
+import { Routes,RouterModule } from '@angular/router';
+import { ProductAddComponent } from './components/product-add/product-add.component';
+import { FormsModule } from '@angular/forms';
+
+const routes : Routes = [
+  {path:'', component:HomeComponent},
+  {path:'admin/product', component: ProductListComponent},
+  {path:'admin/product/addproduct', component: ProductAddComponent},
+  {path: 'admin/product/update/:id', component:ProductAddComponent }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    ProductListComponent,
+    HeaderAdminComponent,
+    ProductAddComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
