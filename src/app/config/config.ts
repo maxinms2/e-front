978a8d97e1bcaec681a1229102be74a11ad8cada
Edgar Mejia <you@example.com>
2024-07-08 22:55:16 -0600

    Certificados ssl

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -1,2 +1,2 @@
-//export const BACKEND_URL='http://159.54.153.176:8085';
-export const BACKEND_URL='http://localhost:8085';
+export const BACKEND_URL='https://marketfit.duckdns.org:8085';
+//export const BACKEND_URL='http://localhost:8085';
